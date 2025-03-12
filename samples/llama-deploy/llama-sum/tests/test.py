8b19edd8c08f7d9d413e67cda50a8a7a052c67e9
Árpád Csepi <csepi.arpad@outlook.com>
2025-03-12 15:39:52 +0100

    chore(samples): format and remove unused imports

--- samples/llama-deploy/llama-sum/tests/test.py
+++ samples/llama-deploy/llama-sum/tests/test.py
@@ -1,9 +1,6 @@
 # SPDX-FileCopyrightText: Copyright (c) 2025 Cisco and/or its affiliates.
 # SPDX-License-Identifier: Apache-2.0
 
-import json
-import os
-import sys
 import time
 
 from llama_deploy import ControlPlaneConfig, LlamaDeployClient
@@ -20,7 +17,5 @@ v = result.split()
 # v1 + v2 = v3
 # we check it the sum returned is correct
 sum = int(v[0]) + int(v[2])
-assert v[4] == str(
-    sum
-), f"Got a wrong results. Expected {str(sum)}, received {v[4]}"
+assert v[4] == str(sum), f"Got a wrong results. Expected {str(sum)}, received {v[4]}"
 print("test succeded")
