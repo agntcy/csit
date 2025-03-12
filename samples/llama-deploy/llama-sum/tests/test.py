02202013293f93a07dc498e67ef027815272c20e
Árpád Csepi <csepi.arpad@outlook.com>
2025-03-12 15:45:05 +0100

    refactor(samples): introduce function in tests

--- samples/llama-deploy/llama-sum/tests/test.py
+++ samples/llama-deploy/llama-sum/tests/test.py
@@ -4,15 +4,24 @@
 
 from llama_deploy import ControlPlaneConfig, LlamaDeployClient
 
-# points to deployed control plane
-client = LlamaDeployClient(ControlPlaneConfig())
+result = None
 
-session = client.create_session()
-result = session.run("sum", max=10)
-v = result.split()
-# expected string in the form of
-# v1 + v2 = v3
-# we check it the sum returned is correct
-sum = int(v[0]) + int(v[2])
-assert v[4] == str(sum), f"Got a wrong results. Expected {str(sum)}, received {v[4]}"
-print("test succeded")
+
+def run_test():
+    # points to deployed control plane
+    client = LlamaDeployClient(ControlPlaneConfig())
+
+    session = client.create_session()
+    result = session.run("sum", max=10)
+    v = result.split()
+    # expected string in the form of
+    # v1 + v2 = v3
+    # we check it the sum returned is correct
+    sum = int(v[0]) + int(v[2])
+    assert v[4] == str(sum), (
+        f"Got a wrong results. Expected {str(sum)}, received {v[4]}"
+    )
+    print("test succeded")
+
+
+run_test()
