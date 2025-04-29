52072da2985c57d494ccb4e6aa1274abf20a042f
Giovanni Conte <giconte@cisco.com>
2025-04-29 16:46:05 +0200

    marketing campaing refectoring
    Signed-off-by: Giovanni Conte <giconte@cisco.com>

--- integrations/agntcy-apps/tools/wfsm_runner.py
+++ integrations/agntcy-apps/tools/wfsm_runner.py
@@ -3,6 +3,7 @@ import sys
 import subprocess
 import logging
 import yaml
+import argparse
 
 # Configure logging
 logging.basicConfig(
@@ -11,7 +12,26 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 
-def run_wfsm_binary(working_dir: str, wfsm_bin_path: str, log_file: str) -> bool:
+
+def parse_arguments():
+    """
+    Command-line arguments parser.
+    """
+    parser = argparse.ArgumentParser(
+        description="Run the wfsm binary and validate environment files",
+        usage=("The --working-dir has to be the path to the directory where the agentic application is located.\n" +
+            "All other paths are relative to that folder.")
+    )
+    parser.add_argument("-w", "--working-dir", required=True, help="Working directory where to run the wfsm CLI.")
+    parser.add_argument("-b", "--wfsm-bin-path", required=True, help="Path to where the wfsm binary is located.")
+    parser.add_argument("-l", "--log-file", required=True, help="Path to the log file that will be created.")
+    parser.add_argument("-m", "--manifest", required=True, help="Path to the model file.")
+    parser.add_argument("-e", "--env-file", required=True, help="Path to the environment file.")
+    parser.add_argument("-f", "--env-file-example", required=True, help="Path to the example YAML env file.")
+    return parser.parse_args()
+
+
+def run_wfsm_binary(working_dir: str, wfsm_bin_path: str, log_file: str, model_file: str, env_file: str) -> bool:
     """
     Starts the wfsm process in the background, writes logs to the file, and continues execution.
 
@@ -19,6 +39,8 @@ def run_wfsm_binary(working_dir: str, wfsm_bin_path: str, log_file: str) -> bool
         working_dir (str): The working directory where the wfsm binary is located.
         wfsm_bin_path (str): Path to the wfsm binary.
         log_file (str): Path to the log file.
+        model_file (str): Path to the model file.
+        env_file (str): Path to the environment file.
 
     Returns:
         bool: True if the process was started successfully, False otherwise.
@@ -30,8 +52,8 @@ def run_wfsm_binary(working_dir: str, wfsm_bin_path: str, log_file: str) -> bool
         command = [
             wfsm_bin_path,
             "deploy",
-            "-m", "./deploy/marketing-campaign.json",
-            "-e", "../../marketing_campaign_cfg_yaml.env"
+            "-m", model_file,
+            "-e", env_file
         ]
 
         logger.info(f"Starting the wfsm process: {' '.join(command)}")
@@ -71,10 +93,10 @@ def flatten_keys(data, parent_key=""):
             keys.append(full_key)
     return keys
 
-def validate_env_file(working_dir: str, env_file: str, example_file: str) -> bool:
+def validate_env_file(working_dir: str, env_file: str, example_env_file: str) -> bool:
     """
-    Validates that the env file contains the keys required by the marketing campaign
-    and ensures no keys have empty values.
+    Validates that the env file contains the keys required by the application and if they are
+    the same as the example file. It checks for missing keys, extra keys, and empty values.
 
     Args:
         env_file (str): Path to the environment file.
@@ -87,7 +109,7 @@ def validate_env_file(working_dir: str, env_file: str, example_file: str) -> boo
         os.chdir(working_dir)
 
         # Load keys from the example YAML file
-        with open(example_file, "r") as f:
+        with open(example_env_file, "r") as f:
             example_data = yaml.safe_load(f)
         example_keys = set(flatten_keys(example_data))
 
@@ -139,19 +161,25 @@ def get_nested_value(data, key):
     return data
 
 if __name__ == "__main__":
-    working_dir = os.path.dirname(os.path.abspath(__file__)) + "/agentic-apps/marketing-campaign"
-    wfsm_bin_path = "../../wfsm"
-    log_file = "../../wfsm.log"
-    env_file = "../../marketing_campaign_cfg_yaml.env"
-    example_file = "./deploy/marketing_campaign_example.yaml"
+    args = parse_arguments()
+    working_dir = args.working_dir
+    try:
+        # If the path is relative, construct the absolute path
+        if not os.path.isabs(working_dir):
+            script_dir = os.path.dirname(os.path.abspath(__file__))
+            if not working_dir.startswith("./") and not working_dir.startswith("../"):
+                working_dir = f"./{working_dir}"
+            working_dir = os.path.abspath(os.path.join(script_dir, working_dir))
 
-    # Validate the env file
-    if not validate_env_file(working_dir, env_file, example_file):
-        logger.error("Validation of the env file failed. Please fix the issues and try again.")
-        sys.exit(1)
-    logger.info("Validation of keys in the env file passed.")
+        # Validate the env file
+        if not validate_env_file(working_dir, args.env_file, args.env_file_example):
+            raise RuntimeError("Validation of the env file failed. Please fix the issues and try again.")
+        logger.info("Validation of env file passed.")
 
-    # Execute wfsm
-    if not run_wfsm_binary(working_dir, wfsm_bin_path, log_file):
-        logger.error("Failed to start the wfsm process.")
+        # Execute wfsm
+        if not run_wfsm_binary(working_dir, args.wfsm_bin_path, args.log_file, args.manifest, args.env_file):
+            raise RuntimeError("Failed to start the wfsm process.")
+
+    except Exception as e:
+        logger.error(e)
         sys.exit(1)
