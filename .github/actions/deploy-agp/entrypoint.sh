9f15f4c3120d8fb47a869631ec61a4290f9c15d0
Peter Balogh <p.balogh.sa@gmail.com>
2025-05-08 14:50:20 +0200

    test
    Signed-off-by: Peter Balogh <p.balogh.sa@gmail.com>

--- .github/actions/deploy-agp/entrypoint.sh
+++ .github/actions/deploy-agp/entrypoint.sh
@@ -8,6 +8,7 @@ KIND_CLUSTER_NAMESPACE=$4
 
 # Define the path to the kubeconfig in the mounted workspace
 KUBECONFIG="/github/workspace/.kube/config"
+kubectl get pods -A
 
 echo "Deploying with gateway image tag: $GATEWAY_IMAGE_TAG"
 echo "Using chart version: $GATEWAY_CHART_TAG"
@@ -15,6 +16,7 @@ echo "To cluster: $KIND_CLUSTER_NAME"
 echo "In namespace: $KIND_CLUSTER_NAMESPACE"
 
 ls /apps
+which task
 
 task
 
