8f3fa6fb2d6400b528ec49eebf8259a08478f829
Peter Balogh <p.balogh.sa@gmail.com>
2025-05-15 16:15:33 +0200

    feat(agp): intial MCP proxy test
    Signed-off-by: Peter Balogh <p.balogh.sa@gmail.com>

--- integrations/agntcy-agp/tests/mcp_porxy_test.go
+++ integrations/agntcy-agp/tests/mcp_porxy_test.go
@@ -15,6 +15,7 @@ import (
 	batchv1 "k8s.io/api/batch/v1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/intstr"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
 )
@@ -48,19 +49,20 @@ var _ = ginkgo.Describe("MCP over AGP test", func() {
 		namespace = os.Getenv("NAMESPACE")
 	})
 
-	ginkgo.Context("AGP native MCP server", func() {
+	ginkgo.Context("AGP native MCP server", ginkgo.Ordered, func() {
 		// The MCP server is AGP-native and works on top of AGP using it as transport.
 		// The client can address the MCP server as if it was a normal agent.
-		ginkgo.It("Deploy MCP time server with AGP support", func() {
+		ginkgo.BeforeAll(func() {
+			podName := "mcp-server-time"
 			pod := &corev1.Pod{
 				ObjectMeta: metav1.ObjectMeta{
-					Name:      "mcp-servert-time",
+					Name:      podName,
 					Namespace: namespace,
 				},
 				Spec: corev1.PodSpec{
 					Containers: []corev1.Container{
 						{
-							Name:  "mcp-server-time",
+							Name:  podName,
 							Image: mcpServerTimeImage,
 							Args: []string{
 								"--local-timezone",
@@ -68,11 +70,6 @@ var _ = ginkgo.Describe("MCP over AGP test", func() {
 								"--config",
 								`{"endpoint":"http://agntcy-agp:46357","tls":{"insecure":true}}`,
 							},
-							// Ports: []corev1.ContainerPort{
-							// 	{
-							// 		ContainerPort: 8000,
-							// 	},
-							// },
 						},
 					},
 					RestartPolicy: corev1.RestartPolicyAlways,
@@ -83,6 +80,12 @@ var _ = ginkgo.Describe("MCP over AGP test", func() {
 			createdPod, err := clientset.CoreV1().Pods(namespace).Create(context.TODO(), pod, metav1.CreateOptions{})
 			gomega.Expect(err).NotTo(gomega.HaveOccurred(), "failed to create MCP time server pod")
 
+			// Register cleanup to run after all the spec is done
+			ginkgo.DeferCleanup(func(ctx context.Context) {
+				err := clientset.CoreV1().Pods(namespace).Delete(ctx, createdPod.Name, metav1.DeleteOptions{})
+				gomega.Expect(err).NotTo(gomega.HaveOccurred(), "failed to delete pod")
+			})
+
 			// Wait for pod to be running
 			err = waitForPodRunning(clientset, namespace, createdPod.Name, 120*time.Second)
 			gomega.Expect(err).NotTo(gomega.HaveOccurred(), createdPod)
@@ -90,9 +93,10 @@ var _ = ginkgo.Describe("MCP over AGP test", func() {
 
 		ginkgo.It("Create Llamaindex time agent Job", func() {
 			var backOffLimit int32 = 2
+			jobName := "llamaindex-time-agent"
 			job := &batchv1.Job{
 				ObjectMeta: metav1.ObjectMeta{
-					Name:      "llamaindex-time-agent",
+					Name:      jobName,
 					Namespace: namespace,
 				},
 				Spec: batchv1.JobSpec{
@@ -100,7 +104,7 @@ var _ = ginkgo.Describe("MCP over AGP test", func() {
 						Spec: corev1.PodSpec{
 							Containers: []corev1.Container{
 								{
-									Name:  "llamaindex-time-agent",
+									Name:  jobName,
 									Image: llamaindexTimeAgentImage,
 									Args: []string{
 										"--city",
@@ -132,16 +136,160 @@ var _ = ginkgo.Describe("MCP over AGP test", func() {
 			createdJob, err := clientset.BatchV1().Jobs(namespace).Create(context.TODO(), job, metav1.CreateOptions{})
 			gomega.Expect(err).NotTo(gomega.HaveOccurred(), "failed to create Llamaindext time agent job")
 
+			// Register cleanup to run after this spec completes
+			ginkgo.DeferCleanup(func(ctx context.Context) {
+				deletePolicy := metav1.DeletePropagationBackground
+				err := clientset.BatchV1().Jobs(namespace).Delete(ctx, createdJob.Name, metav1.DeleteOptions{PropagationPolicy: &deletePolicy})
+				gomega.Expect(err).NotTo(gomega.HaveOccurred(), "failed to delete job")
+			})
+
 			// Wait for job to be succeded
 			err = waitForJobCompletion(clientset, namespace, createdJob.Name, 120*time.Second)
 			gomega.Expect(err).NotTo(gomega.HaveOccurred(), createdJob)
 		})
 	})
 
-	ginkgo.Context("MCP server via MCP proxy", func() {
+	ginkgo.Context("MCP server via MCP proxy", ginkgo.Ordered, func() {
 		// The MCP server works on top of SSE and we can access it using the MCP proxy
-		ginkgo.It("Deploy MCP time server without AGP support", func() {
-			// TODO pbalogh-sa implement
+		ginkgo.BeforeAll(func() {
+			pod := &corev1.Pod{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "mcp-server-time",
+					Namespace: namespace,
+					Labels: map[string]string{
+						"app": "mcp-server-time",
+					},
+				},
+				Spec: corev1.PodSpec{
+					Containers: []corev1.Container{
+						{
+							Name:  "mcp-server-time",
+							Image: mcpServerTimeImage,
+							Args: []string{
+								"--local-timezone",
+								"America/New_York",
+								"--config",
+								`{"endpoint":"http://agntcy-agp:46357","tls":{"insecure":true}}`,
+								"--transport",
+								"sse",
+							},
+							Ports: []corev1.ContainerPort{
+								{
+									ContainerPort: 8000,
+								},
+							},
+						},
+					},
+					RestartPolicy: corev1.RestartPolicyAlways,
+				},
+			}
+			// Create the pod
+			fmt.Println("Creating pod...")
+			createdPod, err := clientset.CoreV1().Pods(namespace).Create(context.TODO(), pod, metav1.CreateOptions{})
+			gomega.Expect(err).NotTo(gomega.HaveOccurred(), "failed to create MCP time server pod")
+
+			// Register cleanup to run after all the spec is done
+			ginkgo.DeferCleanup(func(ctx context.Context) {
+				err := clientset.CoreV1().Pods(namespace).Delete(ctx, createdPod.Name, metav1.DeleteOptions{})
+				gomega.Expect(err).NotTo(gomega.HaveOccurred(), "failed to delete pod")
+			})
+
+			// Wait for pod to be running
+			err = waitForPodRunning(clientset, namespace, createdPod.Name, 120*time.Second)
+			gomega.Expect(err).NotTo(gomega.HaveOccurred(), createdPod)
+
+			// Define the service for MCP server
+			service := &corev1.Service{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "mcp-server",
+					Namespace: namespace,
+				},
+				Spec: corev1.ServiceSpec{
+					Selector: map[string]string{
+						"app": "mcp-server-time",
+					},
+					Ports: []corev1.ServicePort{
+						{
+							Protocol:   corev1.ProtocolTCP,
+							Port:       8000,
+							TargetPort: intstr.FromInt(8000),
+						},
+					},
+					Type: corev1.ServiceTypeClusterIP,
+				},
+			}
+			// Create the secice
+			fmt.Println("Creating service...")
+			createdService, err := clientset.CoreV1().Services(namespace).Create(context.TODO(), service, metav1.CreateOptions{})
+			gomega.Expect(err).NotTo(gomega.HaveOccurred(), "failed to create MCP time server pod")
+
+			// Register cleanup to run after all the spec is done
+			ginkgo.DeferCleanup(func(ctx context.Context) {
+				err := clientset.CoreV1().Services(namespace).Delete(ctx, createdService.Name, metav1.DeleteOptions{})
+				gomega.Expect(err).NotTo(gomega.HaveOccurred(), "failed to delete service")
+			})
+		})
+
+		ginkgo.It("Create Llamaindex time agent Job", func() {
+			var backOffLimit int32 = 2
+			job := &batchv1.Job{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "llamaindex-time-agent",
+					Namespace: namespace,
+				},
+				Spec: batchv1.JobSpec{
+					Template: corev1.PodTemplateSpec{
+						Spec: corev1.PodSpec{
+							Containers: []corev1.Container{
+								{
+									Name:  "llamaindex-time-agent",
+									Image: llamaindexTimeAgentImage,
+									Args: []string{
+										"--city",
+										"New York",
+										"--config",
+										`{"endpoint":"http://agntcy-agp:46357","tls":{"insecure":true}}`,
+										"--mcp-server-organization",
+										"org",
+										"--mcp-server-namespace",
+										"mcp",
+										"--mcp-server-name",
+										"proxy",
+									},
+									Env: []corev1.EnvVar{
+										{
+											Name:  "AZURE_OPENAI_ENDPOINT",
+											Value: azure_openapi_endpoint,
+										},
+										{
+											Name:  "AZURE_OPENAI_API_KEY",
+											Value: azure_openapi_api_key,
+										},
+									},
+								},
+							},
+							RestartPolicy: corev1.RestartPolicyNever,
+						},
+					},
+					BackoffLimit: &backOffLimit,
+				},
+			}
+
+			// Create the job
+			fmt.Println("Creating job...")
+			createdJob, err := clientset.BatchV1().Jobs(namespace).Create(context.TODO(), job, metav1.CreateOptions{})
+			gomega.Expect(err).NotTo(gomega.HaveOccurred(), "failed to create Llamaindext time agent job")
+
+			// Register cleanup to run after this spec completes
+			ginkgo.DeferCleanup(func(ctx context.Context) {
+				deletePolicy := metav1.DeletePropagationBackground
+				err := clientset.BatchV1().Jobs(namespace).Delete(ctx, createdJob.Name, metav1.DeleteOptions{PropagationPolicy: &deletePolicy})
+				gomega.Expect(err).NotTo(gomega.HaveOccurred(), "failed to delete job")
+			})
+
+			// Wait for job to be succeded
+			err = waitForJobCompletion(clientset, namespace, createdJob.Name, 120*time.Second)
+			gomega.Expect(err).NotTo(gomega.HaveOccurred(), createdJob)
 		})
 	})
 })
