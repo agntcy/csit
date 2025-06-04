2b3dc50efcd32593f89b0861a155127f87570daf
Peter Balogh <p.balogh.sa@gmail.com>
2025-06-04 12:22:10 +0200

    refactor: use slim_bindings
    Signed-off-by: Peter Balogh <p.balogh.sa@gmail.com>

--- integrations/agntcy-agp/agentic-apps/autogen_agent/autogen_agent.py
+++ integrations/agntcy-agp/agentic-apps/autogen_agent/autogen_agent.py
@@ -19,30 +19,30 @@ async def run_agent(message, address, iterations):
     remote_namespace = "default"
     remote_agent = "langchain"
 
-    # create new gateway object
-    gateway = await slim_bindings.Gateway.new(local_organization, local_namespace, local_agent)
+    # create new participant object
+    participant = await slim_bindings.Slim.new(local_organization, local_namespace, local_agent)
 
-    # Connect to remote gateway server
+    # Connect to remote slim server
     print(f"connecting to: {address}")
-    _ = await gateway.connect({"endpoint": address, "tls": {"insecure": True}})
+    _ = await participant.connect({"endpoint": address, "tls": {"insecure": True}})
 
     # Get the local agent instance from env
     instance = "autogen_instance"
 
-    async with gateway:
+    async with participant:
         if message:
             # Create a route to the remote ID
-            await gateway.set_route(remote_organization, remote_namespace, remote_agent)
+            await participant.set_route(remote_organization, remote_namespace, remote_agent)
 
             # create a session
-            session = await gateway.create_session(
+            session = await participant.create_session(
                 slim_bindings.PySessionConfiguration.RequestResponse()
             )
 
             for i in range(0, iterations):
                 try:
                     # Send the message
-                    await gateway.publish(
+                    await participant.publish(
                         session,
                         message.encode(),
                         remote_organization,
@@ -52,7 +52,7 @@ async def run_agent(message, address, iterations):
                     print(f"{instance} sent:", message)
 
                     # Wait for a reply
-                    session_info, msg = await gateway.receive(session=session.id)
+                    session_info, msg = await participant.receive(session=session.id)
                     print(
                         f"{instance.capitalize()} received (from session {session_info.id}):",
                         f"{msg.decode()}",
@@ -64,7 +64,7 @@ async def run_agent(message, address, iterations):
         else:
             # Wait for a message and reply in a loop
             while True:
-                session_info, _ = await gateway.receive()
+                session_info, _ = await participant.receive()
                 print(
                     f"{instance.capitalize()} received a new session:",
                     f"{session_info.id}",
@@ -73,7 +73,7 @@ async def run_agent(message, address, iterations):
                 async def background_task(session_id):
                     while True:
                         # Receive the message from the session
-                        session, msg = await gateway.receive(session=session_id)
+                        session, msg = await participant.receive(session=session_id)
                         print(
                             f"{instance.capitalize()} received (from session {session_id}):",
                             f"{msg.decode()}",
@@ -87,7 +87,7 @@ async def run_agent(message, address, iterations):
                         result.inner_messages
                         weather_question = result.inner_messages[-1].content[-1].content.split(":")
                         if weather_question[0] == "WEATHER":
-                            await gateway.publish_to(session, weather_question[1].encode())
+                            await participant.publish_to(session, weather_question[1].encode())
                             print(f"{instance.capitalize()} replies:", weather_question[1])
 
                 asyncio.create_task(background_task(session_info.id))
@@ -95,11 +95,11 @@ async def run_agent(message, address, iterations):
 
 async def main():
     parser = argparse.ArgumentParser(description="Command line client for message passing.")
-    parser.add_argument("-g", "--gateway", type=str, help="Gateway address.", default="http://127.0.0.1:12345")
+    parser.add_argument("-s", "--slim", type=str, help="Slim address.", default="http://127.0.0.1:12345")
     parser.add_argument("-m", "--message", type=str, help="Message to send.")
     parser.add_argument("-i", "--iterations",type=int,help="Number of messages to send, one per second.", default=1)
     args = parser.parse_args()
-    await run_agent(args.message, args.gateway, args.iterations)
+    await run_agent(args.message, args.slim, args.iterations)
 
 
 if __name__ == "__main__":
--- integrations/agntcy-agp/agentic-apps/langchain_agent/langchain_agent.py
+++ integrations/agntcy-agp/agentic-apps/langchain_agent/langchain_agent.py
@@ -22,30 +22,30 @@ async def run_agent(message, address, iterations):
     remote_namespace = "default"
     remote_agent = "autogen"
 
-    # create new gateway object
-    gateway = await slim_bindings.Gateway.new(local_organization, local_namespace, local_agent)
+    # create new participant object
+    participant = await slim_bindings.Slim.new(local_organization, local_namespace, local_agent)
 
-    # Connect to remote gateway server
+    # Connect to remote slim server
     print(f"connecting to: {address}")
-    _ = await gateway.connect({"endpoint": address, "tls": {"insecure": True}})
+    _ = await participant.connect({"endpoint": address, "tls": {"insecure": True}})
 
     # Get the local agent instance from env
     instance = "langchain_instance"
 
-    async with gateway:
+    async with participant:
         if message:
             # Create a route to the remote ID
-            await gateway.set_route(remote_organization, remote_namespace, remote_agent)
+            await participant.set_route(remote_organization, remote_namespace, remote_agent)
 
             # create a session
-            session = await gateway.create_session(
+            session = await participant.create_session(
                 slim_bindings.PySessionConfiguration.FireAndForget()
             )
 
             for i in range(0, iterations):
                 try:
                     # Send the message
-                    await gateway.publish(
+                    await participant.publish(
                         session,
                         message.encode(),
                         remote_organization,
@@ -55,7 +55,7 @@ async def run_agent(message, address, iterations):
                     print(f"{instance} sent:", message)
 
                     # Wait for a reply
-                    session_info, msg = await gateway.receive(session=session.id)
+                    session_info, msg = await participant.receive(session=session.id)
                     print(
                         f"{instance.capitalize()} received (from session {session_info.id}):",
                         f"{msg.decode()}",
@@ -71,7 +71,7 @@ async def run_agent(message, address, iterations):
         else:
             # Wait for a message and reply in a loop
             while True:
-                session_info, _ = await gateway.receive()
+                session_info, _ = await participant.receive()
                 print(
                     f"{instance.capitalize()} received a new session:",
                     f"{session_info.id}",
@@ -80,7 +80,7 @@ async def run_agent(message, address, iterations):
                 async def background_task(session_id):
                     while True:
                         # Receive the message from the session
-                        session, msg = await gateway.receive(session=session_id)
+                        session, msg = await participant.receive(session=session_id)
                         print(
                             f"{instance.capitalize()} received (from session {session_id}):",
                             f"{msg.decode()}",
@@ -92,10 +92,10 @@ async def run_agent(message, address, iterations):
 async def main():
     parser = argparse.ArgumentParser(description="Command line client for message passing.")
     parser.add_argument("-m", "--message", type=str, help="Message to send.")
-    parser.add_argument("-g", "--gateway", type=str, help="Gateway address.", default="http://127.0.0.1:12345")
+    parser.add_argument("-s", "--slim", type=str, help="Slim address.", default="http://127.0.0.1:12345")
     parser.add_argument("-i", "--iterations",type=int,help="Number of messages to send, one per second.", default=1)
     args = parser.parse_args()
-    await run_agent(args.message, args.gateway, args.iterations)
+    await run_agent(args.message, args.slim, args.iterations)
 
 if __name__ == "__main__":
     try:
