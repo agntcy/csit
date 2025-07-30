7faf04dcf5727140d05d67d0a851b05d671c815c
Árpád Csepi <csepi.arpad@outlook.com>
2025-07-30 13:42:00 +0200

    chore(integrations): update test cases
    Signed-off-by: Árpád Csepi <csepi.arpad@outlook.com>

--- integrations/agntcy-dir/tests/list_agent_test.go
+++ integrations/agntcy-dir/tests/list_agent_test.go
@@ -146,8 +146,8 @@ var _ = ginkgo.Describe("Agntcy agent list tests", func() {
 				}
 
 			},
-			ginkgo.Entry("list with one label", []string{"Natural Language Understanding"}, true),
-			ginkgo.Entry("list with two labes", []string{"Natural Language Understanding", "Fact Extraction"}, true),
+			ginkgo.Entry("list with one label", []string{"Natural Language Processing"}, true),
+			ginkgo.Entry("list with two labes", []string{"Natural Language Processing", "Natural Language Processing"}, true), // NOTE: The samples jsons only contains one label
 			ginkgo.Entry("list with non-existing label", []string{"Lorem ipsum dolor sit amet"}, false),
 		)
 	})
